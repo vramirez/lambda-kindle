760910d5879e43fa5f3f8e422e78c3d04add8d17
Victor Ramirez <ramirez.vmanuel@gmail.com>
2019-12-05 07:22:33 -0500

    Little adjustments

--- amazon_price.py
+++ amazon_price.py
@@ -8,7 +8,9 @@ Created on Sat Sep 21 11:47:10 2019
 
 from lxml import html
 import requests
-#from exceptions import ValueError
+import decimal
+from datetime import datetime
+import  boto3
 from time import sleep
 from random import randint
 
@@ -17,13 +19,16 @@ def randomHeader():
     vec.append('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36' )
     vec.append('Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0')
     vec.append('Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0')
-    
+    vec.append('Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0')
+    vec.append('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/77.0.3865.90 Chrome/77.0.3865.90 Safari/537.36')
+    vec.append('Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0')
+    vec.append('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/78.0.3904.108 Chrome/78.0.3904.108 Safari/537.36')
     return vec[randint(0,len(vec)-1)]
 
 def getBookId(url):
     vec=iter(str.split(url,"/"))
     for pos in vec:
-        if pos == "dp":
+        if pos == "dp" or pos=="product":
             return next(vec)
     return None
         
@@ -35,7 +40,7 @@ def parse(url):
         # Retrying for failed requests
         for i in range(20):
             # Generating random delays
-            sleep(randint(1,3))
+            sleep(randint(1,5))
             # Adding verify=False to avold ssl related issues
             response = requests.get(url, headers=headers, verify=True)
 
@@ -45,7 +50,6 @@ def parse(url):
                 XPATH_ORIGINAL_PRICE = '//td[contains(text(),"Digital List Price")]/following-sibling::td/text()'
                 RAW_SALE_PRICE = doc.xpath(XPATH_SALE_PRICE)
                 RAW_ORIGINAL_PRICE = doc.xpath(XPATH_ORIGINAL_PRICE)
-                #print(len(RAW_NAME))
                 
                 SALE_PRICE = RAW_SALE_PRICE[0] if RAW_SALE_PRICE else None
                 ORIGINAL_PRICE = ''.join(RAW_ORIGINAL_PRICE).strip()[1:] if RAW_ORIGINAL_PRICE else None
@@ -54,7 +58,7 @@ def parse(url):
                     ORIGINAL_PRICE = SALE_PRICE
                 # retrying in case of captcha
                 if not SALE_PRICE:
-                    raise ValueError('captcha')
+                    print('captcha')
                     continue
 
                 data = {
@@ -75,5 +79,19 @@ def parse(url):
         print(e)
 
 
-def write(url, email):
-    return 'hole'
+def generateItem(url, email):
+    #
+    obj={}
+    obj['ebookid']=getBookId(url)
+    obj['userid']=email
+    obj['url']=url
+    obj['timestamp']=datetime.now().isoformat()
+    obj['price']=decimal.Decimal(parse(url)['SALE_PRICE'])
+    return obj
+
+def writeItem(item):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('ebooks')
+    response = table.put_item(Item=item)
+
+#def get    
